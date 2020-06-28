adc31b10ce787fdbb12deca0d7d6767048baf9ac
dehan <58659547+dehan97@users.noreply.github.com>
2020-06-28 15:03:47 +0800

    created straits_times_scrape.py

--- master.py
+++ master.py
@@ -1,19 +1,18 @@
 import datetime as dt
-import pandas as pd
+import pandas
 from twitterscraper import query_tweets
 import twitterscraper as ts
+import requests
 
-begin_date = dt.date(2020,5,1)
-end_date = dt.date(2020,6,28)
-
-limit = 1000
-lang = "english"
-
-tweets = ts.query.query_tweets_from_user("awscloud")
-#, begindate=begin_date,enddate=end_date, limit=limit, lang=lang)
-
-df = pd.DataFrame(t.__dict__ for t in tweets)
-pd.set_option("display.max_rows", None, "display.max_columns", None)
-print(df.head())
-
-asfbgaskjbgfaskbnfkj
+# begin_date = dt.date(2020,5,1)
+# end_date = dt.date(2020,6,28)
+#
+# limit = 1000
+# lang = "english"
+#
+# tweets = ts.query.query_tweets_from_user("awscloud")
+# #, begindate=begin_date,enddate=end_date, limit=limit, lang=lang)
+#
+# df = pd.DataFrame(t.__dict__ for t in tweets)
+# pd.set_option("display.max_rows", None, "display.max_columns", None)
+# print(df.head())
---
+++ straits_times_scrape.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv('st_scrape.csv')
+print(df.head())
